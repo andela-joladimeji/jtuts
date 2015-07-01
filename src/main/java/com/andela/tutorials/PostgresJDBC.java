3ca6e2abb7f1eb47725a8cfe8fd1f1595f6b15a0
Olajumoke Oladimeji <jumoke5ng@yahoo.com>
2015-07-01 11:36:34 +0100

    Database Connection using JDBC

--- src/main/java/com/andela/tutorials/PostgresJDBC.java
+++ src/main/java/com/andela/tutorials/PostgresJDBC.java
@@ -1,8 +1,14 @@
 package com.andela.tutorials;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class PostgresJDBC {
 
-		/**
+	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
@@ -13,6 +19,43 @@ public class PostgresJDBC {
 			e.printStackTrace();
 			return;
 		}
+
+		Connection connection = null;
+
+		try {
+			connection = DriverManager.getConnection(
+					"jdbc:postgresql://127.0.0.1:5432/jtuts", "postgres",
+					"postgres");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Connection to database server stablished");
+
+		Statement statement = null;
+		try {
+			statement = connection.createStatement();
+			ResultSet resutlSet = statement
+					.executeQuery("SELECT * FROM settings");
+
+			if (resutlSet.next()) {
+				System.out.println("Version => " + resutlSet.getString(1));
+				System.out.println("Owner   => " + resutlSet.getString(2));
+			}
+
+			ResultSet userResult = statement
+					.executeQuery("SELECT * FROM users");
+			while (userResult.next()) {
+				System.out.println("ID:" + userResult.getString(1));
+				System.out.println("Company:" + userResult.getString(2));
+				System.out.println("Email" + userResult.getString(3));
+				System.out.println("Full Name" + userResult.getString(4));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 }
