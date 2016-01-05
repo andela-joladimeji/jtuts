64add5020ef70d17a378250e7dab65041b01f3b5
Olajumoke Oladimeji <jumoke5ng@yahoo.com>
2016-01-05 11:44:28 +0100

    Pushing final code

--- src/main/java/com/andela/tutorials/ArrayUtil.java
+++ src/main/java/com/andela/tutorials/ArrayUtil.java
@@ -1,6 +1,7 @@
 package com.andela.tutorials;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -63,6 +64,31 @@ public class ArrayUtil {
 	public static Integer sumFirstAndLast(Set<Integer> set) {
 		TreeSet<Integer> sortedSet = new TreeSet<Integer>(set);
 		return Integer.sum(sortedSet.first(), sortedSet.last());
+	}
+
+	public static LinkedList convertToLinkedList(List list) {
+		return new LinkedList(list);
+	}
+
+	public static HashMap convertToHashamp(Map map) {
+		return new HashMap(map);
+	}
+
+	public static HashMap<String, Object> convertFromArrayList(List<Object> list) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		for (Object obj : list) {
+			// process conversion
+			map.put(obj.toString(), obj);
+		}
+		return map;
+	}
+
+	public static List convertFromHashMap(HashMap map) {
+		return new ArrayList(map.values());
+	}
+
+	public static List listFromArray(String[] array) {
+		return new ArrayList<String>(Arrays.asList(array));
 	}
 
 	/*
